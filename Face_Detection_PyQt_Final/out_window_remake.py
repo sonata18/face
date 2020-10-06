ceff8ed52385b66d128a28fe45455e1c3675e1dc
Augmented Startups <reigngt09@gmail.com>
2020-10-06 15:21:53 +0200

    Update README.md

--- Face_Detection_PyQt_Final/out_window_remake.py
+++ Face_Detection_PyQt_Final/out_window_remake.py
@@ -1,14 +1,14 @@
 import sys
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.uic import loadUi
-from PyQt5.QtCore import pyqtSlot, QTimer, QDate
+from PyQt5.QtCore import pyqtSlot, QTimer, QDate, Qt
 from PyQt5.QtWidgets import QDialog, QMessageBox, QApplication
 import cv2
+import face_recognition
 import numpy as np
 import datetime
 import os
 import csv
-import face_recognition
 
 
 class Ui_OutputDialog(QDialog):
@@ -16,12 +16,8 @@ class Ui_OutputDialog(QDialog):
         super(Ui_OutputDialog, self).__init__()
         loadUi("./outputwindow.ui", self)
 
-        # 修改时间更新部分
-        self.timer = QTimer(self)  # 创建定时器
-        self.timer.timeout.connect(self.update_time)  # 将定时器的超时信号连接到更新时间函数
-        self.timer.start(1000)  # 每秒更新一次
-
-        # 更新时间
+        # Update time
+        # TODO: 时间的实时更新
         now = QDate.currentDate()
         current_date = now.toString('ddd dd MMMM yyyy')
         current_time = datetime.datetime.now().strftime("%I:%M %p")
@@ -29,29 +25,6 @@ class Ui_OutputDialog(QDialog):
         self.Time_Label.setText(current_time)
 
         self.image = None
-
-        # Initialize variables for face recognition
-        self.class_names = []
-        self.encode_list = []
-        self.TimeList1 = []
-        self.TimeList2 = []
-
-        # Take photo button click event
-        self.TakePhotoButton.clicked.connect(self.take_photo)
-
-    @pyqtSlot()
-    def take_photo(self):
-        ret, frame = self.capture.read()  # Capture a frame from the camera
-        if ret:
-            # 弹出输入姓名对话框
-            name, ok = QLineEdit.getText(QInputDialog(), 'Enter Name', 'Please enter your name:')
-            if ok:
-                # 生成带有姓名的文件名
-                filename = os.path.join('Face_Detection_PyQt_Final/ImagesAttendance', f'{name}.jpg')
-                cv2.imwrite(filename, frame)  # Save the image
-                QMessageBox.information(self, "Take Photo", f"Photo has been saved as {filename}")
-        else:
-            QMessageBox.warning(self, "Take Photo", "Failed to capture photo.")
 
     @pyqtSlot()
     def startVideo(self, camera_name):
@@ -63,20 +36,31 @@ class Ui_OutputDialog(QDialog):
             self.capture = cv2.VideoCapture(int(camera_name))
         else:
             self.capture = cv2.VideoCapture(camera_name)
-        self.timer = QTimer(self)  # 创建定时器
+        self.timer = QTimer(self)  # Create Timer
         path = 'ImagesAttendance'
         if not os.path.exists(path):
             os.mkdir(path)
-
+        # known face encoding and known face name list
+        images = []
+        self.class_names = []
+        self.encode_list = []
+        self.TimeList1 = []
+        self.TimeList2 = []
         attendance_list = os.listdir(path)
 
+        # print(attendance_list)
         for cl in attendance_list:
             cur_img = cv2.imread(f'{path}/{cl}')
+            images.append(cur_img)
             self.class_names.append(os.path.splitext(cl)[0])
-            self.encode_list.append(face_recognition.face_encodings(cur_img)[0])
-
-        self.timer.timeout.connect(self.update_frame)  # 将定时器的超时信号连接到输出函数
-        self.timer.start(10)  # 以10ms的频率发射超时()信号
+        for img in images:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            boxes = face_recognition.face_locations(img)
+            encodes_cur_frame = face_recognition.face_encodings(img, boxes)[0]
+            # encode = face_recognition.face_encodings(img)[0]
+            self.encode_list.append(encodes_cur_frame)
+        self.timer.timeout.connect(self.update_frame)  # Connect timeout to the output function
+        self.timer.start(10)  # emit the timeout() signal at x=40ms
 
     def face_rec_(self, frame, encode_list_known, class_names):
         """
@@ -87,6 +71,7 @@ class Ui_OutputDialog(QDialog):
         """
 
         # csv
+
         def mark_attendance(name):
             """
             :param name: detected face known or unknown one
@@ -109,7 +94,10 @@ class Ui_OutputDialog(QDialog):
                             self.HoursLabel.setText('Measuring')
                             self.MinLabel.setText('')
 
+                            # self.CalculateElapse(name)
+                            # print('Yes clicked and detected')
                             self.Time1 = datetime.datetime.now()
+                            # print(self.Time1)
                             self.ClockInButton.setEnabled(True)
                         else:
                             print('Not clicked.')
@@ -128,6 +116,7 @@ class Ui_OutputDialog(QDialog):
                             self.NameLabel.setText(name)
                             self.StatusLabel.setText('Clocked Out')
                             self.Time2 = datetime.datetime.now()
+                            # print(self.Time2)
 
                             self.ElapseList(name)
                             self.TimeList2.append(datetime.datetime.now())
@@ -146,12 +135,13 @@ class Ui_OutputDialog(QDialog):
         # face recognition
         faces_cur_frame = face_recognition.face_locations(frame)
         encodes_cur_frame = face_recognition.face_encodings(frame, faces_cur_frame)
-
+        # count = 0
         for encodeFace, faceLoc in zip(encodes_cur_frame, faces_cur_frame):
             match = face_recognition.compare_faces(encode_list_known, encodeFace, tolerance=0.50)
             face_dis = face_recognition.face_distance(encode_list_known, encodeFace)
             name = "unknown"
             best_match_index = np.argmin(face_dis)
+            # print("s",best_match_index)
             if match[best_match_index]:
                 name = class_names[best_match_index].upper()
                 y1, x2, y2, x1 = faceLoc
@@ -184,18 +174,28 @@ class Ui_OutputDialog(QDialog):
                     if field in row:
                         if field == 'Clock In':
                             if row[0] == name:
+                                # print(f'\t ROW 0 {row[0]}  ROW 1 {row[1]} ROW2 {row[2]}.')
                                 Time1 = (datetime.datetime.strptime(row[1], '%y/%m/%d %H:%M:%S'))
                                 self.TimeList1.append(Time1)
                         if field == 'Clock Out':
                             if row[0] == name:
+                                # print(f'\t ROW 0 {row[0]}  ROW 1 {row[1]} ROW2 {row[2]}.')
                                 Time2 = (datetime.datetime.strptime(row[1], '%y/%m/%d %H:%M:%S'))
                                 self.TimeList2.append(Time2)
+                                # print(Time2)
 
     def update_frame(self):
         ret, self.image = self.capture.read()
         self.displayImage(self.image, self.encode_list, self.class_names, 1)
 
     def displayImage(self, image, encode_list, class_names, window=1):
+        """
+        :param image: frame from camera
+        :param encode_list: known face encoding list
+        :param class_names: known face names
+        :param window: number of window
+        :return:
+        """
         image = cv2.resize(image, (640, 480))
         try:
             image = self.face_rec_(image, encode_list, class_names)
@@ -213,11 +213,6 @@ class Ui_OutputDialog(QDialog):
         if window == 1:
             self.imgLabel.setPixmap(QPixmap.fromImage(outImage))
             self.imgLabel.setScaledContents(True)
-
-    @pyqtSlot()
-    def update_time(self):
-        current_time = datetime.datetime.now().strftime("%I:%M:%S %p")
-        self.Time_Label.setText(current_time)
 
 
 if __name__ == "__main__":
--- README.md
+++
@@ -1,8 +0,0 @@
-# Face-Recogntion-PyQt
-
-Watch the full tutorial here: http://bit.ly/facepyqt
-
-# Learn more AI and OpenCV GUI Design?
-
-Enroll in our YOLOv4 PyQt Course here : 
-https://www.augmentedstartups.com/yolov4-course
